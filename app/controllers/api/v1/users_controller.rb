d8965b9929feeb01834e2754290ab66a2f4b0726
franco-canedo <francanedo1@hotmail.com>
2021-01-20 00:19:06 -0600

    api v1 serializers commented out

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create]
+    # skip_before_action :authorized, only: [:create]
  
-  def profile
-    render json: { user: UserSerializer.new(current_user) }, status: :accepted
-  end
+#   def profile
+#     render json: { user: UserSerializer.new(current_user) }, status: :accepted
+#   end
 end 
