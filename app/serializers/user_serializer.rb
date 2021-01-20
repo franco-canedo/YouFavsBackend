43ea2d15a203c56a28e44121a70cae63c45ae552
franco-canedo <francanedo1@hotmail.com>
2021-01-19 19:14:50 -0600

    serializer typo fixed

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
-class UserSerializer < ActiveModel::Serializer
+class UserSerializer < ActiveModel::Serializers
     attributes :id, :uid, :email, :name, :image_url, :google_token, :google_refresh_token
 end
